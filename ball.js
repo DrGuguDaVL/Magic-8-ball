7148a53bbb6f104f5c31a02b24fafe4cf141f97c
DrGuguDaVL <gustavo.vilela.veiga@escola.pr.gov.br>
2025-08-20 22:57:35 -0300

    e

--- ball.js
+++ ball.js
@@ -1,21 +1,22 @@
-ball = Math.floor(Math.random() * 10);
- document.write("<h1>" + response + "</h1>");
+ let ball = Math.floor(Math.random() * 10);
+ let answer = ""
+answer ==("<h1>" + response + "</h1>");
 if(ball==1){
-  document.write("Yes - definitely.");
+ answer ==("Yes - definitely.");
 } else if (ball==2){
-  document.write("It is decidedly so.");
+ answer ==("It is decidedly so.");
 } else if (ball==3){
-  document.write("Without a doubt.");
+ answer ==("Without a doubt.");
 } else if (ball==4){
-  document.write("Reply hazy, try again.");
+ answer ==("Reply hazy, try again.");
 } else if (ball==5){
-  document.write("Ask again later.");
+ answer ==("Ask again later.");
 } else if (ball==6){
-  document.write("Better not tell you now.");
+ answer ==("Better not tell you now.");
 } else if (ball==7){
-  document.write("My sources say no.");
+ answer ==("My sources say no.");
 } else if (ball==8){
-  document.write("Outlook not so good.");
+ answer ==("Outlook not so good.");
 } else if (ball==9){
-  document.write("Very doubtful.");
+ answer ==("Very doubtful.");
 }
